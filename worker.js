1156c8806c61c0b81e1c8353ebc42e0ec98e8731
dagimexample-max <dagimexample@gmail.com>
2026-02-04 05:27:23 +0300

    Reduce message time check from 300 to 30 seconds

--- worker.js
+++ worker.js
@@ -63,7 +63,7 @@ export default {
         const msgCheck = payload.message || payload.callback_query?.message;
         if (msgCheck && msgCheck.date) {
           const currentTime = Math.floor(Date.now() / 1000);
-          if (currentTime - msgCheck.date > 300) { 
+          if (currentTime - msgCheck.date > 30) { 
             return new Response("OK", { status: 200 });
           }
         }
